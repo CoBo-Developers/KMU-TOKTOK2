a4eb265b0676912f2841a5a12574be757951a6fb
Seungkyu-Han <trust1204@gmail.com>
2024-07-15 21:14:15 +0900

    feat: add getKakaoOauth

--- auth/src/main/kotlin/cobo/auth/service/oauth/impl/KakaoOauthServiceImpl.kt
+++ auth/src/main/kotlin/cobo/auth/service/oauth/impl/KakaoOauthServiceImpl.kt
@@ -7,9 +7,12 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.RequestEntity
 import org.springframework.stereotype.Service
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.client.RestTemplate
+import java.net.URI
 
 @Service
 class KakaoOauthServiceImpl(
@@ -18,9 +21,25 @@ class KakaoOauthServiceImpl(
     private val oauthRepository: OauthRepository
 ) : OauthService {
 
-    private val kakaoAccessTokenServer = "https://kauth.kakao.com/oauth/token"
+    private final val kakaoAccessTokenServer = "https://kauth.kakao.com/oauth/token"
+    private final val kakaoUserInfoServer =  "https://kapi.kakao.com/v2/user/me"
+
     override fun getOauth(accessToken: String): Oauth {
-        TODO("Not yet implemented")
+
+        val restTemplate = RestTemplate()
+
+        val httpHeaders = HttpHeaders()
+        httpHeaders.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
+        httpHeaders.add("Authorization", "Bearer $accessToken")
+
+        val kakaoUserInfo =  restTemplate.exchange(
+            RequestEntity<Any>(httpHeaders, HttpMethod.POST, URI.create(kakaoUserInfoServer)),
+            KakaoUserInfo::class.java
+        ).body
+
+        val kakaoUserId = kakaoUserInfo?.id ?: 0L
+
+        return oauthRepository.findByOauthId(kakaoUserId).orElseThrow()
     }
 
     override fun getAccessToken(code: String, redirectUri: String): String {
@@ -41,8 +60,30 @@ class KakaoOauthServiceImpl(
         return restTemplate.postForObject(kakaoAccessTokenServer, HttpEntity(httpBody, httpHeaders), KakaoAccessToken::class.java)?.accessToken ?: ""
     }
 
-    private class KakaoAccessToken(
+    private data class KakaoAccessToken(
         @JsonProperty("access_token")
-        val accessToken: String,
+        val accessToken: String
+    )
+
+    private data class KakaoUserInfo(
+        @JsonProperty("id") val id: Long,
+        @JsonProperty("connected_at") val connectedAt: String,
+        @JsonProperty("properties") val properties: Properties,
+        @JsonProperty("kakao_account") val kakaoAccount: KakaoAccount
+    )
+
+    private data class Properties(
+        @JsonProperty("nickname") val nickname: String
+    )
+
+    private data class KakaoAccount(
+        @JsonProperty("profile_nickname_needs_agreement") val profileNicknameNeedsAgreement: Boolean,
+        @JsonProperty("profile") val profile: Profile
+    )
+
+    private data class Profile(
+        @JsonProperty("nickname") val nickname: String,
+        @JsonProperty("is_default_nickname") val isDefaultNickname: Boolean,
+        @JsonProperty("profile_image_url") val profileImageUrl:String
     )
 }
