91af55fcb8e9594c29f9d975ea55a13fb4d35f5a
Seungkyu-Han <trust1204@gmail.com>
2024-07-15 13:02:48 +0900

    fix: fix Log Error

--- auth/src/main/kotlin/cobo/auth/config/response/CoBoResponse.kt
+++ auth/src/main/kotlin/cobo/auth/config/response/CoBoResponse.kt
@@ -1,7 +1,6 @@
 package cobo.auth.config.response
 
 import cobo.auth.config.LogFilter
-import org.apache.logging.log4j.LogManager
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatusCode
