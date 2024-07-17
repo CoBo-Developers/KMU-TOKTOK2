89f892f77b71af6fb433bc5e91e2eb8e44a7334b
Seungkyu-Han <trust1204@gmail.com>
2024-07-18 01:03:25 +0900

    test: add register test private member

--- auth/src/test/kotlin/cobo/auth/auth/AuthRegisterTest.kt
+++ auth/src/test/kotlin/cobo/auth/auth/AuthRegisterTest.kt
@@ -1,6 +1,11 @@
 package cobo.auth.auth
 
 import cobo.auth.config.jwt.JwtTokenProvider
+import cobo.auth.data.entity.Oauth
+import cobo.auth.data.entity.User
+import cobo.auth.data.enums.OauthTypeEnum
+import cobo.auth.data.enums.RegisterStateEnum
+import cobo.auth.data.enums.RoleEnum
 import cobo.auth.repository.OauthRepository
 import cobo.auth.repository.UserRepository
 import cobo.auth.service.AuthService
@@ -15,6 +20,53 @@ class AuthRegisterTest(
     @Autowired private val oauthRepository: OauthRepository,
     @Autowired private val userRepository: UserRepository
 ) {
+
+    private val kakaoUser = User(
+        id = -1,
+        studentId = null,
+        role = RoleEnum.STUDENT,
+        registerState = RegisterStateEnum.INACTIVE
+    )
+
+    private val naverUser = User(
+        id = -2,
+        studentId = null,
+        role = RoleEnum.STUDENT,
+        registerState = RegisterStateEnum.INACTIVE
+    )
+
+    private val googleUser = User(
+        id = -3,
+        studentId = null,
+        role = RoleEnum.STUDENT,
+        registerState = RegisterStateEnum.INACTIVE
+    )
+
+    private val kakaoOauth = Oauth(
+        id = -1,
+        user = kakaoUser,
+        oauthId = "testKakaoUser",
+        oauthType = OauthTypeEnum.KAKAO,
+        accessToken = null
+    )
+
+    private val naverOauth = Oauth(
+        id = -2,
+        user = naverUser,
+        oauthId = "testNaverUser",
+        oauthType = OauthTypeEnum.NAVER,
+        accessToken = null
+    )
+
+    private val googleOauth = Oauth(
+        id = -3,
+        user = googleUser,
+        oauthId = "testGoogleUser",
+        oauthType = OauthTypeEnum.GOOGLE,
+        accessToken = null
+    )
+
+
 
     companion object {
         @JvmStatic
