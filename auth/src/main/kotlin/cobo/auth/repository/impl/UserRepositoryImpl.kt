4a429cabf15f726e620aab88a03bba908c3fc8b7
Seungkyu-Han <trust1204@gmail.com>
2024-07-20 00:35:22 +0900

    fix: UserRepository SQL Error

--- auth/src/main/kotlin/cobo/auth/repository/impl/UserRepositoryImpl.kt
+++ auth/src/main/kotlin/cobo/auth/repository/impl/UserRepositoryImpl.kt
@@ -4,6 +4,7 @@ import cobo.auth.data.entity.User
 import cobo.auth.data.enums.RegisterStateEnum
 import cobo.auth.data.enums.RoleEnum
 import cobo.auth.repository.custom.UserRepositoryCustom
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
@@ -16,22 +17,28 @@ class UserRepositoryImpl(
 ) : UserRepositoryCustom {
 
     override fun findByStudentIdWithJDBC(studentId: String): Optional<User> {
-        return Optional.ofNullable(
-            jdbcTemplate.queryForObject("SELECT * FROM user WHERE student_id = ?", {rs, _ -> userRowMapper(rs)}, studentId)
-        )
+        return try{
+            Optional.ofNullable(
+                jdbcTemplate.queryForObject("SELECT * FROM user WHERE student_id = ?", { rs, _ -> userRowMapper(rs)}, studentId)
+            )
+        }catch(e: EmptyResultDataAccessException){
+            Optional.empty()
+        }
     }
 
     @Transactional
     override fun updateStudentIdWithJDBC(id: Int, studentId: String, registerStateEnum: RegisterStateEnum) {
-        jdbcTemplate.update("UPDATE user SET student_id = ? WHERE id = ? and register_state = ?", studentId, id, registerStateEnum.value)
+        jdbcTemplate.update("UPDATE user SET student_id = ?, register_state = ?  WHERE id = ?", studentId, registerStateEnum.value, id)
     }
 
     private fun userRowMapper(resultSet: ResultSet): User {
+
         return User(
             id = resultSet.getInt("id"),
             studentId = resultSet.getString("student_id"),
             role = RoleEnum.from(resultSet.getShort("role")),
             registerState = RegisterStateEnum.from(resultSet.getShort("register_state")) ?: RegisterStateEnum.ACTIVE
         )
+
     }
 }
