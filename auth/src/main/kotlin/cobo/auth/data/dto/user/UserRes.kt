c4831f299802994579c3bbf0984cae9d1740e952
Seungkyu-Han <trust1204@gmail.com>
2024-07-21 00:57:16 +0900

    feat: add Get List User API schema

--- auth/src/main/kotlin/cobo/auth/data/dto/user/UserRes.kt
+++ auth/src/main/kotlin/cobo/auth/data/dto/user/UserRes.kt
@@ -1,10 +1,14 @@
 package cobo.auth.data.dto.user
 
 import cobo.auth.data.entity.User
+import io.swagger.v3.oas.annotations.media.Schema
 
 data class UserRes(
+    @Schema(description = "학번")
     val studentId: String?,
+    @Schema(description = "권한")
     val role: String?,
+    @Schema(description = "학번 입력 완료 여부")
     val registerState: String?
 ){
     constructor(user: User) : this(
@@ -16,6 +20,7 @@ data class UserRes(
 
 data class GetUserListRes(
     val users: List<UserRes>,
+    @Schema(description = "전체 유저의 수")
     val totalElements: Long
 ){
     constructor(
