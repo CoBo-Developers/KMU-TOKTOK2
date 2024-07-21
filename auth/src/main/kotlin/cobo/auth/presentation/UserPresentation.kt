7d57ed7b4bc3d1c28e3746894b75f04ac0785e7f
Seungkyu-Han <trust1204@gmail.com>
2024-07-21 20:40:44 +0900

    feat: add Swagger description

--- auth/src/main/kotlin/cobo/auth/presentation/UserPresentation.kt
+++ auth/src/main/kotlin/cobo/auth/presentation/UserPresentation.kt
@@ -9,8 +9,13 @@ import cobo.auth.service.UserService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.Parameters
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
+import org.springframework.web.ErrorResponse
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -25,6 +30,9 @@ class UserPresentation(
         Parameter(name = "page", description = "조회할 페이지의 번호"),
         Parameter(name = "pageSize", description = "한 페이지에 조회할 데이터의 수")
     )
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "조회 성공", content = [Content(schema = Schema(implementation = GetUserListRes::class))])
+    )
     fun getList(
         @RequestParam page: Int,
         @RequestParam pageSize: Int): ResponseEntity<CoBoResponseDto<GetUserListRes>> {
@@ -33,13 +41,20 @@ class UserPresentation(
 
     @PutMapping
     @Operation(summary = "유저 정보 수정")
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "수정 성공")
+    )
     fun patch(@RequestBody putUserReq: PutUserReq): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>> {
         return userService.put(putUserReq)
     }
 
     @GetMapping
     @Operation(summary = "유저 정보 조회")
-
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "조회 성공"),
+        ApiResponse(responseCode = "404", description = "해당하는 유저가 존재하지 않음",
+            content = [Content(schema = Schema(implementation = CoBoResponseDto::class))])
+    )
     fun get(@RequestParam studentId: String): ResponseEntity<CoBoResponseDto<GetUserRes>>{
         return userService.get(studentId)
     }
