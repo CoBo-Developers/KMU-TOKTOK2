766dea0451b4591881568b0ca79f4ba854660b1f
Seungkyu-Han <trust1204@gmail.com>
2024-07-24 23:54:56 +0900

    feat: add Response Description

--- auth/src/main/kotlin/cobo/auth/presentation/AuthPresentation.kt
+++ auth/src/main/kotlin/cobo/auth/presentation/AuthPresentation.kt
@@ -7,6 +7,8 @@ import cobo.auth.service.AuthService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.Parameters
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.HttpHeaders
@@ -78,6 +80,9 @@ class AuthPresentation(
 
     @PatchMapping("/login")
     @Operation(summary = "AccessToken 재발급 API")
+    @ApiResponses(
+        ApiResponse(responseCode = "400", description = "TOKEN IS EMPTY")
+    )
     fun patchLogin(@Parameter(hidden = true) @RequestHeader(name = HttpHeaders.AUTHORIZATION) authorization: String): ResponseEntity<CoBoResponseDto<GetAuthLoginRes>>{
         return authService.patchLogin(authorization)
     }
