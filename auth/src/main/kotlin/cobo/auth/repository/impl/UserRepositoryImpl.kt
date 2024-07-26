68032124cdfed9a70ca24398389215d659e0d9eb
Seungkyu-Han <trust1204@gmail.com>
2024-07-26 13:40:49 +0900

    test: add User Put Test

--- auth/src/main/kotlin/cobo/auth/repository/impl/UserRepositoryImpl.kt
+++ auth/src/main/kotlin/cobo/auth/repository/impl/UserRepositoryImpl.kt
@@ -35,7 +35,7 @@ class UserRepositoryImpl(
     override fun updateUserByStudentIdWithJDBC(studentId: String, role: Short, registerState: Short, newStudentId: String): Int {
         return jdbcTemplate.update("UPDATE user " +
                 "SET role = ?, register_state = ?, student_id = ? " +
-                "WHERE student_id = ?", role, registerState, studentId)
+                "WHERE student_id = ?", role, registerState, newStudentId, studentId)
     }
 
     private fun userRowMapper(resultSet: ResultSet): User {
