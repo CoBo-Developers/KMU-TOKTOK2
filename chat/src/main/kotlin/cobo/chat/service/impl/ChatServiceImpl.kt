f8179747ad1b2fef88c6ba73787141870c84460b
Seungkyu-Han <trust1204@gmail.com>
2024-07-28 18:01:51 +0900

    feat: add student Post API

--- chat/src/main/kotlin/cobo/chat/service/impl/ChatServiceImpl.kt
+++ chat/src/main/kotlin/cobo/chat/service/impl/ChatServiceImpl.kt
@@ -1,8 +1,17 @@
 package cobo.chat.service.impl
 
+import cobo.chat.config.response.CoBoResponse
+import cobo.chat.config.response.CoBoResponseDto
+import cobo.chat.config.response.CoBoResponseStatus
+import cobo.chat.data.dto.student.StudentPostReq
+import cobo.chat.data.entity.Chat
+import cobo.chat.data.entity.ChatRoom
+import cobo.chat.data.enum.ChatStateEnum
 import cobo.chat.repository.ChatRepository
 import cobo.chat.repository.ChatRoomRepository
 import cobo.chat.service.ChatService
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,4 +19,22 @@ class ChatServiceImpl(
     private val chatRoomRepository: ChatRoomRepository,
     private val chatRepository: ChatRepository
 ): ChatService {
+    override fun studentPost(
+        studentPostReq: StudentPostReq,
+        authentication: Authentication
+    ): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>> {
+
+        val studentId: String = authentication.name
+        val chatRoom = ChatRoom(id = studentId, chatStateEnum = ChatStateEnum.WAITING)
+        val chat = Chat(
+            chatRoom = chatRoom,
+            comment = studentPostReq.question,
+            isQuestion = true
+        )
+
+        chatRoomRepository.ifExistUpdateElseInsert(chatRoom)
+        chatRepository.insert(chat)
+
+        return CoBoResponse<CoBoResponseStatus>(CoBoResponseStatus.SUCCESS).getResponseEntity()
+    }
 }
