16d73abc65ee8b44167d92b1798b48bd4bd44432
Seungkyu-Han <trust1204@gmail.com>
2024-07-30 10:43:41 +0900

    feat: add Prof Get API

--- chat/src/main/kotlin/cobo/chat/service/ChatService.kt
+++ chat/src/main/kotlin/cobo/chat/service/ChatService.kt
@@ -3,6 +3,7 @@ package cobo.chat.service
 import cobo.chat.config.response.CoBoResponseDto
 import cobo.chat.config.response.CoBoResponseStatus
 import cobo.chat.data.dto.prof.ProfGetListRes
+import cobo.chat.data.dto.prof.ProfGetElementRes
 import cobo.chat.data.dto.prof.ProfPostReq
 import cobo.chat.data.dto.student.StudentGetElementRes
 import cobo.chat.data.dto.student.StudentPostReq
@@ -18,4 +19,5 @@ interface ChatService {
     fun studentGet(authentication: Authentication): ResponseEntity<CoBoResponseDto<List<StudentGetElementRes>>>
     fun profPost(profPostReq: ProfPostReq): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>>
     fun profGetList(page: Int, pageSize: Int): ResponseEntity<CoBoResponseDto<ProfGetListRes>>
+    fun profGet(studentId: String): ResponseEntity<CoBoResponseDto<List<ProfGetElementRes>>>
 }
