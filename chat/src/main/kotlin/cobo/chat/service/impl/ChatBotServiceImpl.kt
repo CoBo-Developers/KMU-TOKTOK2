bbf5cd82b15c0dcea0b75a2a34207ae8790a4dda
Seungkyu-Han <trust1204@gmail.com>
2024-07-31 11:35:36 +0900

    feat: add repository in service

--- chat/src/main/kotlin/cobo/chat/service/impl/ChatBotServiceImpl.kt
+++ chat/src/main/kotlin/cobo/chat/service/impl/ChatBotServiceImpl.kt
@@ -1,8 +1,13 @@
 package cobo.chat.service.impl
 
+import cobo.chat.repository.ChatBotChatRepository
 import cobo.chat.service.ChatBotService
 import org.springframework.stereotype.Service
 
 @Service
-class ChatBotServiceImpl: ChatBotService {
+class ChatBotServiceImpl(
+    private val chatBotChatRepository: ChatBotChatRepository
+): ChatBotService {
+
+
 }
