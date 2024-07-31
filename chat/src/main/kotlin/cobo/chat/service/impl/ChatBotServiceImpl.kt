3026be0023d5205deb6d8dd62d3ec976f142021b
Seungkyu-Han <trust1204@gmail.com>
2024-07-31 11:54:20 +0900

    feat: add Post ChatBot save in DB

--- chat/src/main/kotlin/cobo/chat/service/impl/ChatBotServiceImpl.kt
+++ chat/src/main/kotlin/cobo/chat/service/impl/ChatBotServiceImpl.kt
@@ -5,9 +5,9 @@ import cobo.chat.config.response.CoBoResponseDto
 import cobo.chat.config.response.CoBoResponseStatus
 import cobo.chat.data.dto.chatBot.ChatBotPostReq
 import cobo.chat.data.dto.chatBot.ChatBotPostRes
+import cobo.chat.data.entity.ChatBotChat
 import cobo.chat.repository.ChatBotChatRepository
 import cobo.chat.service.ChatBotService
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Service
@@ -20,8 +20,18 @@ class ChatBotServiceImpl(
         chatBotPostReq: ChatBotPostReq,
         authentication: Authentication
     ): ResponseEntity<CoBoResponseDto<ChatBotPostRes>> {
+
+        val studentId = authentication.name
+
+        val answer = getAnswerFromChatGPT(chatBotPostReq.question)
+
+        chatBotChatRepository.save(
+            ChatBotChat(
+                id = null, studentId = studentId, answer = answer, question = chatBotPostReq.question, createdAt = null
+        ))
+
         return CoBoResponse(
-            ChatBotPostRes(getAnswerFromChatGPT(chatBotPostReq.question)),
+            ChatBotPostRes(answer),
             CoBoResponseStatus.SUCCESS
         ).getResponseEntity()
     }
