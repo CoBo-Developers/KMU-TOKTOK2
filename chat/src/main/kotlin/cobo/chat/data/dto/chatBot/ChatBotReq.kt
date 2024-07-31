361b8537bdcd57390baac995e233e849102006f3
Seungkyu-Han <trust1204@gmail.com>
2024-07-31 12:34:57 +0900

    feat: add Dto description

--- chat/src/main/kotlin/cobo/chat/data/dto/chatBot/ChatBotReq.kt
+++ chat/src/main/kotlin/cobo/chat/data/dto/chatBot/ChatBotReq.kt
@@ -1,5 +1,10 @@
 package cobo.chat.data.dto.chatBot
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class ChatBotPostReq(
+    @JsonProperty("question")
+    @Schema(description = "답변", example = "자장면입니다.")
     val question: String
 )
--- chat/src/main/kotlin/cobo/chat/data/dto/chatBot/ChatBotRes.kt
+++ chat/src/main/kotlin/cobo/chat/data/dto/chatBot/ChatBotRes.kt
@@ -1,13 +1,18 @@
 package cobo.chat.data.dto.chatBot
 
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 
 data class ChatBotPostRes(
+    @Schema(description = "질문", example = "오늘의 메뉴는?")
     val answer: String
 )
 
 data class ChatBotGetElementRes(
+    @Schema(description = "작성 일자", example = "2024-07-31T12:23:03.396369")
     val createdAt: LocalDateTime,
+    @Schema(description = "답변", example = "자장면입니다.")
     val answer: String,
+    @Schema(description = "질문", example = "오늘의 메뉴는?")
     val question: String,
 )
