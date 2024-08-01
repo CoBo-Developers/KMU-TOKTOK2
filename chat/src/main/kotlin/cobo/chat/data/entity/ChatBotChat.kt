dfbcf26a14a76d22c436b788e411c06a4af98789
Seungkyu-Han <trust1204@gmail.com>
2024-08-02 00:36:10 +0900

    fix: change column length to save GPT answer

--- chat/src/main/kotlin/cobo/chat/data/entity/ChatBotChat.kt
+++ chat/src/main/kotlin/cobo/chat/data/entity/ChatBotChat.kt
@@ -13,8 +13,10 @@ data class ChatBotChat(
     @Column(length = 20)
     val studentId: String,
 
+    @Column(length = 5000)
     val question: String,
 
+    @Column(length = 5000)
     val answer: String,
 
     @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
