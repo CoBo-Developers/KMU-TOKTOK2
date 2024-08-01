139652a434bbddd9bc87a58edc3109a1a9525fa6
Seungkyu-Han <trust1204@gmail.com>
2024-08-02 00:36:40 +0900

    feat: add GPT Dto

---
+++ chat/src/main/kotlin/cobo/chat/data/dto/chatGPT/ChatGPTRes.kt
@@ -0,0 +1,59 @@
+package cobo.chat.data.dto.chatGPT
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class ChatGPTRes(
+    @JsonProperty("id")
+    val id: String,
+
+    @JsonProperty("object")
+    val objectName: String,
+
+    @JsonProperty("created")
+    val created: Long,
+
+    @JsonProperty("model")
+    val model: String,
+
+    @JsonProperty("choices")
+    val choices: List<Choice>,
+
+    @JsonProperty("usage")
+    val usage: Usage,
+
+    @JsonProperty("system_fingerprint")
+    val systemFingerprint: String?,
+)
+
+data class Choice(
+    @JsonProperty("index")
+    val index: Int,
+
+    @JsonProperty("message")
+    val message: ChatGPTResMessage,
+
+    @JsonProperty("logprobs")
+    val logprobs: String?,
+
+    @JsonProperty("finish_reason")
+    val finishReason: String,
+)
+
+data class ChatGPTResMessage(
+    @JsonProperty("role")
+    val role: String,
+
+    @JsonProperty("content")
+    val content: String,
+)
+
+data class Usage(
+    @JsonProperty("prompt_tokens")
+    val promptTokens: Int,
+
+    @JsonProperty("completion_tokens")
+    val completionTokens: Int,
+
+    @JsonProperty("total_tokens")
+    val totalTokens: Int
+)
