0ed3cb068da5a8686544ed77a1d87d385990566d
Seungkyu-Han <trust1204@gmail.com>
2024-08-03 00:32:44 +0900

    feat: add Writing Repository

--- writing/src/main/kotlin/cobo/writing/service/impl/WritingServiceImpl.kt
+++ writing/src/main/kotlin/cobo/writing/service/impl/WritingServiceImpl.kt
@@ -1,8 +1,11 @@
 package cobo.writing.service.impl
 
+import cobo.writing.repository.WritingRepository
 import cobo.writing.service.WritingService
 import org.springframework.stereotype.Service
 
 @Service
-class WritingServiceImpl: WritingService {
+class WritingServiceImpl(
+    private val writingRepository : WritingRepository
+): WritingService {
 }
