3d7cd1a9856bd316880c17fd17437cbbd4c8e58b
Seungkyu-Han <trust1204@gmail.com>
2024-08-02 15:16:10 +0900

    feat: add springboot start

---
+++ writing/src/main/kotlin/cobo/writing/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package cobo.writing
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(WritingApplication::class.java)
+    }
+
+}
---
+++ writing/src/test/kotlin/cobo/writing/WritingApplicationTests.kt
@@ -0,0 +1,13 @@
+package cobo.writing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WritingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
