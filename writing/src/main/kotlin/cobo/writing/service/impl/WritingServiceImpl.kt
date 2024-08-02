899eea582f8e50a648c287143d6a8736186072d7
Seungkyu-Han <trust1204@gmail.com>
2024-08-03 00:30:13 +0900

    feat: add Writing Service

---
+++ writing/src/main/kotlin/cobo/writing/service/impl/WritingServiceImpl.kt
@@ -0,0 +1,8 @@
+package cobo.writing.service.impl
+
+import cobo.writing.service.WritingService
+import org.springframework.stereotype.Service
+
+@Service
+class WritingServiceImpl: WritingService {
+}
