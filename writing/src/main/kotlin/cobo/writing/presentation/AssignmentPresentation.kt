da5fe8df434220ad66cf2958bdf1459dd96757bb
Seungkyu-Han <trust1204@gmail.com>
2024-08-04 17:09:56 +0900

    feat: add Assignment Presentation

---
+++ writing/src/main/kotlin/cobo/writing/presentation/AssignmentPresentation.kt
@@ -0,0 +1,15 @@
+package cobo.writing.presentation
+
+import cobo.writing.service.AssignmentService
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/assignment")
+class AssignmentPresentation(
+    private val assignmentService: AssignmentService
+) {
+
+
+}
