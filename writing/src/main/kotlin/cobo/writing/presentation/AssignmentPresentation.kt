8de61e9683164bb360ffa484e2f8ab00ee21e61e
Seungkyu-Han <trust1204@gmail.com>
2024-08-04 17:20:19 +0900

    feat: add post assignment api in presentation

--- writing/src/main/kotlin/cobo/writing/presentation/AssignmentPresentation.kt
+++ writing/src/main/kotlin/cobo/writing/presentation/AssignmentPresentation.kt
@@ -1,6 +1,16 @@
 package cobo.writing.presentation
 
+import cobo.writing.config.response.CoBoResponseDto
+import cobo.writing.config.response.CoBoResponseStatus
+import cobo.writing.data.dto.assignment.AssignmentPostReq
 import cobo.writing.service.AssignmentService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,5 +20,14 @@ class AssignmentPresentation(
     private val assignmentService: AssignmentService
 ) {
 
+    @PostMapping
+    @Operation(summary = "과제 생성")
+    @ApiResponses(
+        ApiResponse(responseCode = "201", description = "성공"),
+    )
+    fun post(
+        @RequestBody assignmentPostReq: AssignmentPostReq): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>>{
+        return ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+    }
 
 }
