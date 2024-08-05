6060371b3b5675d2db42296f4c4afba07d3e52ae
Seungkyu-Han <trust1204@gmail.com>
2024-08-05 15:35:59 +0900

    test: add Assignment Put in middle test

--- writing/src/test/kotlin/cobo/writing/assignment/AssignmentPutTest.kt
+++ writing/src/test/kotlin/cobo/writing/assignment/AssignmentPutTest.kt
@@ -4,6 +4,7 @@ import cobo.writing.data.dto.assignment.AssignmentPutReq
 import cobo.writing.data.entity.Assignment
 import cobo.writing.repository.AssignmentRepository
 import cobo.writing.service.AssignmentService
+import jakarta.persistence.EntityNotFoundException
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -71,6 +72,66 @@ class AssignmentPutTest @Autowired constructor(
         assertEquals(assignmentPutReq.endDate, newAssignment.endDate)
     }
 
+    @Test
+    fun testPutAssignmentInMiddle(){
+        //given
+        val startCount = (1..3).random()
+        for (i in 1..startCount) {
+            val curAssignment = makeTestAssignment()
+            assignmentList.add(assignmentRepository.save(curAssignment))
+        }
 
-    //없는거
+        val assignment = assignmentRepository.save(makeTestAssignment())
+        assignmentList.add(assignment)
+
+        val endCount = (1..3).random()
+        for(i in 1..endCount) {
+            val curAssignment = makeTestAssignment()
+            assignmentList.add(assignmentRepository.save(curAssignment))
+        }
+
+        //when
+        val assignmentPutReq = AssignmentPutReq(
+            id = assignment.id ?: throw NullPointerException("id"),
+            title = UUID.randomUUID().toString(),
+            description = UUID.randomUUID().toString(),
+            score = (1..20).random(),
+            startDate = LocalDate.of(2024, (1..7).random(), (1..20).random()),
+            endDate = LocalDate.of(2024, (8..12).random(), (1..20).random())
+        )
+        assignmentService.put(assignmentPutReq)
+
+        //then
+
+        val newAssignment = assignmentRepository.findById(assignment.id!!).orElseThrow()
+
+        assertEquals(assignment.id, newAssignment.id)
+        assertEquals(assignmentPutReq.title, newAssignment.title)
+        assertEquals(assignmentPutReq.description, newAssignment.description)
+        assertEquals(assignmentPutReq.score, newAssignment.score)
+        assertEquals(assignmentPutReq.startDate, newAssignment.startDate)
+        assertEquals(assignmentPutReq.endDate, newAssignment.endDate)
+
+    }
+
+    @Test
+    fun testPutAssignmentWithNotValidId(){
+        //given
+        val assignmentPutReq = AssignmentPutReq(
+            id = Int.MAX_VALUE / 2,
+            title = UUID.randomUUID().toString(),
+            description = UUID.randomUUID().toString(),
+            score = (1..20).random(),
+            startDate = LocalDate.of(2024, (1..7).random(), (1..20).random()),
+            endDate = LocalDate.of(2024, (8..12).random(), (1..20).random()),
+        )
+
+        //when
+        try{
+            assignmentService.put(assignmentPutReq)
+            assert(false)
+        }catch(entityNotFoundException: EntityNotFoundException){
+            assert(true)
+        }
+    }
 }
