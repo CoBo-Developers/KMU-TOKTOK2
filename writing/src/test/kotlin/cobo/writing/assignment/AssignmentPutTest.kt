10df98bfa4e7e001e5d8f220cf8a5bbe84c9bef3
Seungkyu-Han <trust1204@gmail.com>
2024-08-05 12:42:35 +0900

    test: add Assignment Put test Skeleton

---
+++ writing/src/test/kotlin/cobo/writing/assignment/AssignmentPutTest.kt
@@ -0,0 +1,10 @@
+package cobo.writing.assignment
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class AssignmentPutTest {
+
+}
