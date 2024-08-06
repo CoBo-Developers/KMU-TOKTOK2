5560f59b1d7e393778042468376be1771267be07
Seungkyu-Han <trust1204@gmail.com>
2024-08-06 12:41:59 +0900

    feat: add getAssignment By studentId query

--- writing/src/main/kotlin/cobo/writing/repository/custom/AssignmentRepositoryCustom.kt
+++ writing/src/main/kotlin/cobo/writing/repository/custom/AssignmentRepositoryCustom.kt
@@ -1,4 +1,7 @@
 package cobo.writing.repository.custom
 
+import cobo.writing.data.dto.student.StudentGetListResElement
+
 interface AssignmentRepositoryCustom {
+    fun findByUserWithJDBC(studentId: String?): List<StudentGetListResElement>
 }
