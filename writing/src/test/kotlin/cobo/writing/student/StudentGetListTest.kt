bb91708b9f43a165241d5bd66055fd9dbde0f320
Seungkyu-Han <trust1204@gmail.com>
2024-08-07 15:35:42 +0900

    test: add test student Get list with not submitted

--- writing/src/test/kotlin/cobo/writing/student/StudentGetListTest.kt
+++ writing/src/test/kotlin/cobo/writing/student/StudentGetListTest.kt
@@ -1,5 +1,6 @@
 package cobo.writing.student
 
+import cobo.writing.data.dto.student.StudentGetListResElement
 import cobo.writing.data.entity.Assignment
 import cobo.writing.data.entity.Writing
 import cobo.writing.data.enums.WritingStateEnum
@@ -7,12 +8,20 @@ import cobo.writing.repository.AssignmentRepository
 import cobo.writing.repository.WritingRepository
 import cobo.writing.service.AssignmentService
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.test.annotation.DirtiesContext
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
+import kotlin.test.assertNotNull
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -47,18 +56,66 @@ class StudentGetListTest @Autowired constructor(
         )
     }
 
-    private fun makeTestWriting(assignment: Assignment, state: WritingStateEnum):Writing {
+    private fun makeTestWriting(assignment: Assignment, state: Short):Writing {
         return Writing(
             id = null,
             studentId = studentId,
             assignment = assignment,
             content = UUID.randomUUID().toString(),
-            state = state,
+            state = WritingStateEnum.from(state) ?: WritingStateEnum.NOT_SUBMITTED,
             createdAt = LocalDateTime.now(),
             updatedAt = LocalDateTime.now(),
             submittedAt = LocalDateTime.now()
         )
     }
 
+    private fun makeTestStudent(studentId: String): SecurityContext {
+        val securityContextHolder = SecurityContextHolder.getContext()
+        securityContextHolder.authentication = UsernamePasswordAuthenticationToken(
+            studentId, null, listOf(
+                SimpleGrantedAuthority("STUDENT")
+            ))
+        return securityContextHolder
+    }
+
+    private fun testStudentGetListWithState(state: Short){
+        //given
+        val assignment = makeTestAssignment()
+        assignmentRepository.save(assignment)
+        assignmentList.add(assignment)
+
+        val writing = makeTestWriting(assignment, state)
+        writingRepository.save(writing)
+        writingList.add(writing)
+
+        val securityContextHolder = makeTestStudent(studentId)
+
+        //when
+        val studentGetListRes = assignmentService.studentGetList(securityContextHolder.authentication)
+
+        //then
+
+        assertEquals(HttpStatus.OK, studentGetListRes.statusCode)
+
+        assertNotNull(studentGetListRes.body!!.data)
+
+        val expectedStudentGetListResElement =
+            StudentGetListResElement(
+                id = assignment.id!!,
+                title = assignment.title!!,
+                description = assignment.description!!,
+                score = assignment.score,
+                startDate = assignment.startDate,
+                endDate = assignment.endDate,
+                writingState = state
+            )
+
+        assertEquals(expectedStudentGetListResElement, studentGetListRes.body!!.data!!.assignmentList.last())
+    }
+
+    @Test
+    fun testStudentGetListWithNotSubmitted(){
+        testStudentGetListWithState(WritingStateEnum.NOT_SUBMITTED.value)
+    }
 
 }
