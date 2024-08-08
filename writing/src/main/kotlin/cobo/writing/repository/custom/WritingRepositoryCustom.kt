16e361ceae14944c74374cadf2108f613f8ce04d
Seungkyu-Han <trust1204@gmail.com>
2024-08-08 11:12:46 +0900

    feat: add writing ifExistsUpdateElseInsert Query

--- writing/src/main/kotlin/cobo/writing/repository/custom/WritingRepositoryCustom.kt
+++ writing/src/main/kotlin/cobo/writing/repository/custom/WritingRepositoryCustom.kt
@@ -1,4 +1,8 @@
 package cobo.writing.repository.custom
 
+import cobo.writing.data.entity.Writing
+
 interface WritingRepositoryCustom {
+
+    fun ifExistsUpdateElseInsert(writing: Writing): Int
 }
