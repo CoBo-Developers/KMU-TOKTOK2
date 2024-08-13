4e61b107073def2c23b16baebcdcd44744c78dd0
Seungkyu-Han <trust1204@gmail.com>
2024-08-13 17:53:51 +0900

    feat: add coboResponse

---
+++ file/src/main/kotlin/cobo/file/config/response/CoBoResponseStatus.kt
@@ -0,0 +1,18 @@
+package cobo.file.config.response
+
+enum class CoBoResponseStatus(
+    val code: Int,
+    val message: String
+) {
+
+    SUCCESS(2000, "OK"),
+    NO_DATA_CHANGES(2001, "SUCCESS, BUT NO DATA CHANGES"),
+
+
+    BAD_REQUEST(4000, "BAD REQUEST"),
+    NOT_AUTHORIZATION(4011, "AUTHORIZATION TOKEN IS EMPTY"),
+    NOT_FOUND_USER(4041, "USER NOT FOUND"),
+    CANT_SAVE(4042, "CANT SAVE IN DATABASE"),
+    NOT_FOUND_STUDENT(4043, "TARGET STUDENT NOT FOUND"),
+    CANT_GET_RESOURCES(4044, "CANT GET RESOURCES"),
+}
