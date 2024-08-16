f77e3cff362bcd0976f3cc377a9b6e78219032bb
Seungkyu-Han <trust1204@gmail.com>
2024-08-16 23:40:39 +0900

    feat: add category delete by name query

--- file/src/main/kotlin/cobo/file/repository/CategoryRepository.kt
+++ file/src/main/kotlin/cobo/file/repository/CategoryRepository.kt
@@ -2,8 +2,11 @@ package cobo.file.repository
 
 import cobo.file.data.entity.Category
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 interface CategoryRepository: JpaRepository<Category, Int> {
     fun findByName(oldCategory: String): Optional<Category>
+    @Transactional
+    fun deleteByName(category: String)
 }
