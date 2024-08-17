735d72e881c843b8b6c23d645cc17ef7ae81d48f
Seungkyu-Han <trust1204@gmail.com>
2024-08-18 00:02:55 +0900

    feat: add file post API Skeleton code in presentation

---
+++ file/src/main/kotlin/cobo/file/presentation/ProfessorFilePresentation.kt
@@ -0,0 +1,27 @@
+package cobo.file.presentation
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/professor/category")
+@Tag(name = "파일 관련 API(교수, 개발자만 사용 가능)")
+class ProfessorFilePresentation(
+) {
+
+    @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    @Operation(summary = "파일 업로드 API", description = "교수, 개발자 권한만 사용 가능")
+    @ApiResponses(
+        ApiResponse(responseCode = "201", description = "업로드 성공", content = arrayOf(Content()))
+    )
+    private fun post(): ResponseEntity<HttpStatus> {
+        return ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+    }
+}
